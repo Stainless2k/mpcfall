c09f985eeec12ac2feab610199beaa496ae28f7d
Leon Guenther <leonsechteemailadresse@gmail.com>
2022-08-26 06:37:59 +1000

    reading from txt now

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import waifu2x
 import image_utils
 
 IMAGE_FOLDER: pathlib.Path = pathlib.Path('cards')
+CARDS_TXT: pathlib.Path = pathlib.Path('cards.txt')
 
 
 def download_image(card_name: str):
@@ -19,14 +20,21 @@ def download_image(card_name: str):
 if __name__ == '__main__':
     IMAGE_FOLDER.mkdir(exist_ok=True)
 
-    name = ['brainstorm', 'mox opal']
-    for x in name:
+    with CARDS_TXT.open('r') as file:
+        names = file.readlines()
+
+    for x in names:
+        print('DOWNLOADING: ', x)
         download_image(x)
 
     for x in IMAGE_FOLDER.glob('*.png'):
         output = x.parent / 'scaled' / x.name
         if not output.is_file():
+            print('SCALING: ', x)
             waifu2x.run(input_img_path=str(x), output_img_path=str(x.parent / 'scaled' / x.name))
+        else:
+            print('SKIP SCALING: ', x)
 
     for x in (IMAGE_FOLDER / 'scaled').glob('*.png'):
+        print('BLEEDING: ', x)
         image_utils.add_bleed(x)
