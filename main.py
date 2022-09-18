978b772f9a43f2be675e7d33ab7f688e740513b0
Leon Guenther <leonsechteemailadresse@gmail.com>
2022-09-18 20:17:34 +1000

    added set codes and multi processing

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import multiprocessing
 import pathlib
+from multiprocessing import Pool
+from typing import Optional
 
 import requests
 import scrython
@@ -8,33 +11,56 @@ import image_utils
 
 IMAGE_FOLDER: pathlib.Path = pathlib.Path('cards')
 CARDS_TXT: pathlib.Path = pathlib.Path('cards.txt')
+CPUS_TO_USE = multiprocessing.cpu_count() - 1
 
 
-def download_image(card_name: str):
-    card_url = scrython.cards.Named(exact=card_name).image_uris().get('png')
+def download_image(card_name: str, set_code: Optional[str]):
+    if set_code is not None:
+        try:
+            card_url = scrython.cards.Named(exact=card_name, set=set_code).image_uris().get('png')
+        except scrython.foundation.ScryfallError:
+            card_url = scrython.cards.Named(exact=card_name).image_uris().get('png')
+            set_code = 'NOT_FOUND_' + set_code
+    else:
+        card_url = scrython.cards.Named(exact=card_name).image_uris().get('png')
+        set_code = ''
+
     img_data = requests.get(card_url).content
-    with open(IMAGE_FOLDER / f'{card_name}.png', 'wb') as handler:
+    with open(IMAGE_FOLDER / f'{card_name}_{set_code}.png', 'wb') as handler:
         handler.write(img_data)
+
+
+def scale_images(image_path: pathlib.Path):
+    output_path = image_path.parent / 'scaled' / image_path.name
+    if not output_path.is_file():
+        print('SCALING: ', image_path)
+        waifu2x.run(input_img_path=str(image_path), output_img_path=str(output_path))
+    else:
+        print('SKIP SCALING: ', image_path)
 
 
 if __name__ == '__main__':
     IMAGE_FOLDER.mkdir(exist_ok=True)
+    names_and_set_codes = []
 
     with CARDS_TXT.open('r') as file:
-        names = file.readlines()
+        lines = file.readlines()
+        for line in lines:
+            split = line.split('|', maxsplit=1)
+            if len(split) < 2:
+                names_and_set_codes.append([split[0].strip(), None])
+            else:
+                names_and_set_codes.append([split[0].strip(), split[1].strip()])
 
-    for x in names:
-        print('DOWNLOADING: ', x)
-        download_image(x)
+    for name, code in names_and_set_codes:
+        print('DOWNLOADING: ', f"{name} {code}")
+        download_image(card_name=name, set_code=code)
 
-    for x in IMAGE_FOLDER.glob('*.png'):
-        output = x.parent / 'scaled' / x.name
-        if not output.is_file():
-            print('SCALING: ', x)
-            waifu2x.run(input_img_path=str(x), output_img_path=str(x.parent / 'scaled' / x.name))
-        else:
-            print('SKIP SCALING: ', x)
+    print(f'Staring scaling with {CPUS_TO_USE} cores')
+    downloaded_images = IMAGE_FOLDER.glob('*.png')
+    with Pool(CPUS_TO_USE) as p:
+        p.map(scale_images, downloaded_images)
 
     for x in (IMAGE_FOLDER / 'scaled').glob('*.png'):
-        print('BLEEDING: ', x)
+        print('Bleeding: ', x)
         image_utils.add_bleed(x)
